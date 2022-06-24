2be310b22189305a8f39066f06890320b88759ad
Aquifer1519 <79878869+Aquifer1519@users.noreply.github.com>
2022-06-24 17:22:30 +0000

    day 5 javascript

--- website/index.js
+++ website/index.js
@@ -1,7 +1,21 @@
+const fs = require('fs')
+let output = {}
+fs.readFile('./script.json', 'utf-8', (err, data) => {
+  if (err) throw err
+  output = JSON.parse(data)
+});
+
 let startButton = document.getElementById("startButton");
 
-startButton.onclick = function(){greet()};
+startButton.onclick = function(){playStory()};
 
 function greet(){
     alert("You clicked!");
+}
+
+function playStory(num){
+    document.getElementById("header").hidden =  true;
+    document.getElementById("story").hidden = false;
+    //document.getElementById("story").write(output.story1.text1);
+    //document.getElementById("storyOutput").write("what colors do you see around you")
 }
